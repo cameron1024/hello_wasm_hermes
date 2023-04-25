ab1faa1f5ebfd97ceb11a663e13ed7b562ebca6f
Cameron <cameron.studdstreet@gmail.com>
2023-04-25 16:47:16 +0100

    update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+set -e
 
 cargo build --target wasm32-wasi --release
-wasm-tools component new ./target/wasm32-wasi/release/hello_wasm_hermes.wasm -o hermes-consumer.wasm --adapt ./wasi_snapshot_preview1.reactor.wasm
+
+wasm-tools component new ./target/wasm32-wasi/release/hello_wasm_hermes.wasm -o hermes-consumer.wasm --adapt ./wasi_snapshot_preview1.wasm
 
 echo "hermes consumer written to ./hermes-consumer.wasm"
