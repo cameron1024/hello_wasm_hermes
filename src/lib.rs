ec2f670ba9fb3154dfcae8e7892be8eb20fae25b
Cameron <cameron.studdstreet@gmail.com>
2023-04-25 16:42:49 +0100

    demo using print function

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,11 @@ export_hermes!(Host);
 
 impl Hermes for Host {
     fn process(b: Block) -> bool {
+        let msg = format!("hello from WASM, slot number is {}", b.slot);
+        
+        // this function is defined on the host side in Rust code
+        host::print(&msg);
+
         b.slot % 2 == 0
     }
 }
